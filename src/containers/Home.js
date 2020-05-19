4da9f97746df09a37b88b4a5daa072e3973f551c
yunive <yuniveyissel@gmail.com>
2020-05-18 17:15:51 -0700

    updates to home page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,42 +1,39 @@
-import React from "react";
+import React, {Component} from "react";
 import "./Home.css";
 
-import yunive from "../images/yunive.png"
-import hackd from "../images/hackd.png";
-import ua from "../images/ua.png";
 
+import { Carousel } from "react-bootstrap";
+import kayaking from "../images/kayak.jpeg";
 
-function Home() {
-  return (
-    <div className="Home">
-      <div className = "container">
-        <div className = "row">
-          <img src={yunive} alt="Yunive"/>
-        </div>
-        <div className="row">
+class Home extends Component {
 
-          <div className="parent">
-            <div className="child">
-              <img src={hackd} alt="hackDartmouth"/>
-                <div className="overlay-text">
-                Hack Dartmouth
-                Marketing Lead
-              </div>
-            </div>
-
-            <div className="child">
-              <img src={ua} alt="University of Arizona"/>
-                <div className="overlay-text">
-                Data 7
-                UI/UX designer
-              </div>
-            </div>
-          </div>
-        </div>
-        
-
+  render () {
+    
+    return (
+      <div className= "contain">
+        <Carousel>
+          <Carousel.Item>
+            <img src={kayaking} alt="kayak"/>
+            <Carousel.Caption>
+              Marketing Lead 
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={kayaking} alt="kayak"/>
+            <Carousel.Caption>
+              Marketing Lead 
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+          <img src={kayaking} alt="kayak"/>
+            <Carousel.Caption>
+              Marketing Lead 
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
       </div>
-    </div>
-  );
+    );
+  }
+  
 }
 export default Home;
